d2354c36c94ecf5f96d42f6a74fe1148cda6bbff
salahulbain <salahul.bain@gmail.com>
2022-11-22 11:08:27 +0700

    relayout dashboard admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    @include('includes.meta')
+    <title>@yield('title') | {{ config('app.name', 'Tracer Study') }}</title>
+    @stack('addon-before-style')
+    @include('includes.style')
+    @stack('addon-after-style')
+</head>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+<body>
+    <div id="app">
+        @include('includes.sidebar')
+        <div id="main">
+            @yield('content')
+            @include('includes.footer')
         </div>
-    </body>
+    </div>
+    @stack('addon-before-script')
+    @include('includes.script')
+    @stack('addon-before-script')
+</body>
+
 </html>
