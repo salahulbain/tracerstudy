40a8fe709da7ebb49634bc7aed241cd63c5fb40f
salahulbain <salahul.bain@gmail.com>
2022-11-23 09:46:07 +0700

    add data mahasiswa

---
+++ resources/views/pages/admin/datamahasiswa/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('title','Data Mahasiswa')
+@section('content')
+<header class="mb-3">
+    <a href="#" class="burger-btn d-block d-xl-none">
+        <i class="bi bi-justify fs-3"></i>
+    </a>
+</header>
+
+<div class="page-heading">
+    <h3>Data Mahasiswa</h3>
+</div>
+<div class="page-content">
+    <section class="row">
+        <div class="col-12">
+            tabel data mahasiswa
+        </div>
+    </section>
+</div>
+@endsection
