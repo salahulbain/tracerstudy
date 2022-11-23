63316072e07d6d8c00351b3643e6efa30b947da1
salahulbain <salahul.bain@gmail.com>
2022-11-23 09:58:13 +0700

    add migration data mahasiswa

---
+++ database/migrations/2022_11_23_094733_create_data_mahasiswas_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('kode_pt');
+            $table->bigInteger('kode_prodi');
+            $table->bigInteger('npm')->unique();
+            $table->string('nama_mahasiswa');
+            $table->string('no_hp');
+            $table->string('email')->unique();
+            $table->integer('tahun_lulus');
+            $table->bigInteger('nik');
+            $table->bigInteger('npwp')->nullable();
+            $table->string('tempat_lahir')->nullable();
+            $table->integer('tanggal_lahir')->nullable();
+            $table->text('alamat')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_mahasiswas');
+    }
+};
