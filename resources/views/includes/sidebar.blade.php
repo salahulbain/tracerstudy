1697b214bae2270516670ccd29583828a706e4b5
salahulbain <salahul.bain@gmail.com>
2022-11-24 10:13:59 +0700

    add shadow

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div id="sidebar" class="active">
-    <div class="sidebar-wrapper active">
+    <div class="sidebar-wrapper active shadow">
         <div class="sidebar-header position-relative">
             <div class="d-flex justify-content-between align-items-center">
                 <div class="logo">
