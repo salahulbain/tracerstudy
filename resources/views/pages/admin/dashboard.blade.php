24a153eae92fb260221711f7088e5250718849ac
salahulbain <salahul.bain@gmail.com>
2022-11-29 14:49:06 +0700

    fix title dashboard

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title','Dasboard')
+@section('title','Dashboard')
 @section('content')
 <header class="mb-3">
     <a href="#" class="burger-btn d-block d-xl-none">
