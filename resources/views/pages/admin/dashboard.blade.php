a0ed06b713f86421dd855467d6af8795ec512f88
salahulbain <salahul.bain@gmail.com>
2022-11-30 23:50:27 +0700

    update chart on page dashboard

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -165,7 +165,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4>Visitors Profile</h4>
+                        <h4>Jenis Kelamin</h4>
                     </div>
                     <div class="card-body">
                         <div id="chart-visitors-profile"></div>
