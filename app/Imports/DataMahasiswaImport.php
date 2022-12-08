cd99861b8adc2242628c15baf6afa9aa448814a0
salahulbain <salahul.bain@gmail.com>
2022-12-08 22:17:10 +0700

    add validation data on upload file

--- app/Imports/DataMahasiswaImport.php
+++ app/Imports/DataMahasiswaImport.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\DataMahasiswa;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithUpserts;
@@ -18,14 +19,31 @@ class DataMahasiswaImport implements ToModel, WithHeadingRow, WithUpserts
     {
         return 'npm';
     }
-    
+
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
+        Validator::make($row, [
+            'kode_pt'        => 'required|numeric',
+            'kode_prodi'     => 'required|numeric',
+            'npm'            => 'required|numeric|unique:users,username|unique:data_mahasiswas,npm,NULL,id,deleted_at,NULL',
+            'nama_mahasiswa' => 'required',
+            'no_hp'          => 'required',
+            'email'          => 'required|email|unique:users,email|unique:data_mahasiswas,email,NULL,id,deleted_at,NULL',
+            'tahun_lulus'    => 'required',
+            'nik'            => 'required|numeric|digits:16|unique:data_mahasiswas,nik,NULL,id,deleted_at,NULL',
+            'npwp'           => 'nullable|numeric',
+        ], [
+            'required' => ':attribute tidak boleh kosong',
+            'numeric'  => ':attribute wajib berupa angka',
+            'digits'   => ':attribute wajib berjumlah 16 angka',
+            'unique'   => ':attribute sudah terdaftar',
+        ])->validate();
+
         $data_mahasiswa =  DataMahasiswa::updateOrCreate([
             'kode_pt'        => $row['kode_pt'],
             'kode_prodi'     => $row['kode_prodi'],
@@ -46,7 +64,7 @@ class DataMahasiswaImport implements ToModel, WithHeadingRow, WithUpserts
             'user_id'  => $data_mahasiswa->id,
             'role'     => 'USER',
             'avatar'   => 'default.svg',
-        ],['username','email',],['name','user_id']);
+        ], ['username', 'email',], ['name', 'user_id']);
 
         return $data_mahasiswa;
     }
