bb5b20c5c759ec55d7ed4975d8810bd4c5e8b69a
salahulbain <salahul.bain@gmail.com>
2023-03-05 15:06:04 +0700

    make header button show on everi desktop size

--- resources/views/pages/admin/datamahasiswa/index.blade.php
+++ resources/views/pages/admin/datamahasiswa/index.blade.php
@@ -2,7 +2,7 @@
 @section('title','Data Mahasiswa')
 @section('content')
 <header class="mb-3">
-    <a href="#" class="burger-btn d-block d-xl-none">
+    <a href="#" class="burger-btn d-block">
         <i class="bi bi-justify fs-3"></i>
     </a>
 </header>
