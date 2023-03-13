1402438e7b160bd49085ffcbd303174e84aa8fa2
salahulbain <salahul.bain@gmail.com>
2023-03-13 15:13:05 +0700

    update meta

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>{{ config('app.name', 'Tracer Study') }}</title>
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
+    <meta name="description" content="Tracer Study" />
+    <meta name="keywords" content="tracerstudy" />
     <meta name="author" content="" />
     <meta name="Developer" content="Salahul Bain" />
 
